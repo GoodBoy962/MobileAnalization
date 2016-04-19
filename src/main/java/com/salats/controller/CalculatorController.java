f441e8cea2047dbf1ffceedd532879917692c650
Alexander Pliskin <alexnetmore@gmail.com>
2016-04-19 15:47:03 +0300

    request to service done

--- src/main/java/com/salats/controller/CalculatorController.java
+++ src/main/java/com/salats/controller/CalculatorController.java
@@ -1,8 +1,11 @@
 package com.salats.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -20,15 +23,17 @@ public class CalculatorController {
     }
 
     @RequestMapping(value = "search", method = RequestMethod.POST)
-    public String searchTarifs(HttpServletRequest request) {
+    public String searchTarifs(HttpServletRequest request, Model model) {
         Map map = request.getParameterMap();
         String[] operators = (String[]) map.get("operators[]");
         String[] duration = (String[]) map.get("duration");
         String[] byRussian = (String[]) map.get("by_russia");
         String[] internet = (String[]) map.get("internet");
         String[] sms = (String[]) map.get("sms");
-        
-        return "search";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://unlimtariffs.ru/calculator/?operators%5B%5D=1&operators%5B%5D=3&operators%5B%5D=4&operators%5B%5D=5&duration=1&by_russia=1&internet=2&sms=2.json", String.class);
+        model.addAttribute("r_body", responseEntity.toString());
+        return "results";
     }
 
 }
