a57c1afbcd4c818404eef0e4825a08782c6a8b0b
Alexander Pliskin <alexnetmore@gmail.com>
2016-04-19 13:21:03 +0300

    some changes

---
+++ src/main/java/com/salats/model/Model.java
@@ -0,0 +1,7 @@
+package com.salats.model;
+
+/**
+ * Created by aleksandrpliskin on 19.04.16.
+ */
+public class Model {
+}
---
+++ src/main/java/com/salats/service/impl/PlainServiceImpl.java
@@ -0,0 +1,7 @@
+package com.salats.service.impl;
+
+/**
+ * Created by aleksandrpliskin on 19.04.16.
+ */
+public class PlainServiceImpl {
+}
